e5e32b65e4094238a2f21afd5c18a78092e214c6
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-20 11:52:29 -0600

    Refactor import statement for db module in waste route

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,17 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prismaClientSingleton = () => {
-//   return new PrismaClient();
-// };
-
-// const globalForPrisma = globalThis;
-
-// const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-// export default prisma;
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global;
