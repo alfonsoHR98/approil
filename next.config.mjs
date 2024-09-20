23691efed6653382a7d4cf0431e51f48ef5ece73
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-20 12:12:07 -0600

    Refactor next.config.mjs to include environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_URL: process.env.AUTH_URL,
     DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
   },
 };
 
