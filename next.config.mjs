b7efb30631b3cd976b141602e9d82addc1bd65e9
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-25 17:03:35 -0600

    audit next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+import path from "path";
+
 const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
@@ -7,6 +10,17 @@ const nextConfig = {
     DATABASE_URL: process.env.DATABASE_URL,
     POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "src/components"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    };
+    return config;
   },
 };
 
