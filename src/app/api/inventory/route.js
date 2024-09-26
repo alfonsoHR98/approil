473334e0fdba6cabb89a9f22436a262009725a02
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-26 00:57:40 -0600

    Refactor inventory route to disable caching

--- src/app/api/inventory/route.js
+++ src/app/api/inventory/route.js
@@ -13,7 +13,19 @@ export async function GET(req) {
         warehouse: true,
       },
     });
-    return NextResponse.json(inventory, { status: 200 });
+
+    const response = NextResponse.json(inventory, { status: 200 });
+
+    // Añadir encabezados para deshabilitar la caché
+    response.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Surrogate-Control", "no-store");
+
+    return response;
   } catch (error) {
     console.error("Error fetching inventory:", error);
     return NextResponse.json(
