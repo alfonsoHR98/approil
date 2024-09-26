b6bbe5b378d8b250c57c1c553e291056184f96be
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-26 13:14:41 -0600

    Refactor GET route in stock API to include product and warehouse details in the response

--- src/app/api/stock/route.js
+++ src/app/api/stock/route.js
@@ -2,7 +2,16 @@ import { NextResponse } from "next/server";
 import db from "@lib/db";
 
 export async function GET(req) {
-  const res = await db.stock.findMany();
+  const res = await db.stock.findMany({
+    include: {
+      product: {
+        include: {
+          unit: true,
+        }
+      },
+      warehouse: true,
+    },
+  });
   return NextResponse.json(res, { status: 200 });
 }
 
