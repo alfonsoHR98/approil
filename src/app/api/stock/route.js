7d370986d0382096b6230e54167c45f8b12ac2f0
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-26 12:21:47 -0600

    Refactor inventory route to use stock model

--- src/app/api/stock/route.js
+++ src/app/api/stock/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import db from "@lib/db";
 
 export async function GET(req) {
-  const res = await db.inventory.findMany();
+  const res = await db.stock.findMany();
   return NextResponse.json(res, { status: 200 });
 }
 
