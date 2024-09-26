4b427e20f938ab6e3e4d0d79cba88540a4e82cd9
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-26 02:33:08 -0600

    Refactor inventory route to remove unnecessary code and update response handling

--- src/app/api/inventory/route.js
+++ src/app/api/inventory/route.js
@@ -2,20 +2,7 @@ import { NextResponse } from "next/server";
 import db from "@lib/db";
 
 export async function GET() {
-  const res = await db.inventory.findMany({
-    include: {
-      product: {
-        include: {
-          unit: true, // Incluir la información de la unidad
-        },
-      },
-      warehouse: true,
-    },
-  });
+  const res = await db.inventory.findMany();
   console.log(res);
-  return NextResponse.json(res, {
-    status: 200,
-    headers: { "Cache-Control": "no-store" },
-  });
-
+  return NextResponse.json(res, { status: 200 });
 }
