133f0eb7259d65791004c5aa8d01d994097c21a5
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-26 00:51:52 -0600

    Refactor inventory context provider hierarchy and change inventory context

--- src/app/api/inventory/route.js
+++ src/app/api/inventory/route.js
@@ -2,15 +2,23 @@ import { NextResponse } from "next/server";
 import db from "@lib/db";
 
 export async function GET(req) {
-  const inventory = await db.inventory.findMany({
-    include: {
-      product: {
-        include: {
-          unit: true, // Incluir la información de la unidad
+  try {
+    const inventory = await db.inventory.findMany({
+      include: {
+        product: {
+          include: {
+            unit: true, // Incluir la información de la unidad
+          },
         },
+        warehouse: true,
       },
-      warehouse: true,
-    },
-  });
-  return NextResponse.json(inventory, { status: 200 });
+    });
+    return NextResponse.json(inventory, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching inventory:", error);
+    return NextResponse.json(
+      { error: "Error fetching inventory" },
+      { status: 500 }
+    );
+  }
 }
