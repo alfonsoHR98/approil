2daf1409a530862384e6d2b750a5e69daa54eca1
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-26 12:10:46 -0600

    Add Stock model to the database schema and API route
    This commit adds a new model called Stock to the database schema. The Stock model represents the stock of a product in a warehouse and includes fields such as product_id, warehouse_id, quantity, state, createdAt, and updatedAt.
    
    Additionally, this commit adds a new API route for creating and updating stock. The route allows users to add or update the stock quantity for a specific product in a warehouse. If the stock already exists, the quantity is updated. If the stock does not exist, a new stock entry is created.
    
    The changes include modifications to the following files:
    - prisma/schema.prisma: Added the Stock model definition
    - src/app/api/stock/route.js: Added the POST route for creating and updating stock
    - prisma/migrations/20240926180921_add_stock_model/migration.sql: Added the SQL migration script for creating the Stock table

--- src/app/api/stock/route.js
+++ src/app/api/stock/route.js
@@ -5,3 +5,47 @@ export async function GET(req) {
   const res = await db.inventory.findMany();
   return NextResponse.json(res, { status: 200 });
 }
+
+export async function POST(req) {
+  const data = await req.json();
+
+  const { product_id, warehouse_id, quantity } = data;
+
+  const inventoryFound = await db.stock.findFirst({
+    where: {
+      AND: [{ product_id }, { warehouse_id }],
+    },
+  });
+
+  if (inventoryFound) {
+    // Update the quantity
+    const res = await db.stock.update({
+      where: {
+        id: inventoryFound.id,
+      },
+      data: {
+        quantity: inventoryFound.quantity + quantity,
+      },
+    });
+
+    return NextResponse.json(
+      res,
+      { message: "Cantidad actualizada" },
+      { status: 200 }
+    );
+  }
+
+  const res = await db.stock.create({
+    data: {
+      product_id,
+      warehouse_id,
+      quantity,
+    },
+  });
+
+  return NextResponse.json(
+    res,
+    { message: "Stock actualizado" },
+    { status: 200 }
+  );
+}
