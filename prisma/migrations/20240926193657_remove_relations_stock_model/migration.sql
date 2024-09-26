90898a6e58d115508e659ee853a66655148a4c46
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-26 13:37:46 -0600

    Refactor Stock model relations and GET route in stock API

---
+++ prisma/migrations/20240926193657_remove_relations_stock_model/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Stock" DROP CONSTRAINT "Stock_product_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Stock" DROP CONSTRAINT "Stock_warehouse_id_fkey";
--- src/app/api/stock/route.js
+++ src/app/api/stock/route.js
@@ -2,15 +2,6 @@ import { NextResponse } from "next/server";
 import db from "@lib/db";
 
 export async function GET(req) {
-  const res = await db.stock.findMany({
-    include: {
-      product: {
-        include: {
-          unit: true,
-        }
-      },
-      warehouse: true,
-    },
-  });
+  const res = await db.stock.findMany();
   return NextResponse.json(res, { status: 200 });
 }
