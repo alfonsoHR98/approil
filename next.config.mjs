7926475b91784ffb134b5666a5239ba7e6d6387a
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-09-25 19:20:42 -0600

    modified packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Convierte import.meta.url a __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const nextConfig = {
-  env: {
-    API_URL: process.env.API_URL,
-    AUTH_SECRET: process.env.AUTH_SECRET,
-    AUTH_URL: process.env.AUTH_URL,
-    DATABASE_URL: process.env.DATABASE_URL,
-    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
-    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@components": path.resolve(__dirname, "src/components"),
-      "@context": path.resolve(__dirname, "src/context"),
-      "@app": path.resolve(__dirname, "src/app"),
-      "@lib": path.resolve(__dirname, "src/lib"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
